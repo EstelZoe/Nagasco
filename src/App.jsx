2f5246d28fa1317436e1cab5c7808ad34ce8c699
EstelZoe <estelzoe433@gmail.com>
2026-03-28 09:56:26 -0500

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-5xl font-bold text-orange-500">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
